4eac0f527dd6db3d10dbce2c403845a648a168de
Dreiotech <yonidreio3@gmail.com>
2024-04-29 10:46:21 +0300

    initial commit

---
+++ app/src/main/java/com/kimm/dreioprojectone/ui/theme/screens/Home/Home.kt
@@ -0,0 +1,8 @@
+package com.kimm.dreioprojectone.ui.theme.screens.Home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+}
---
+++ app/src/main/java/com/kimm/dreioprojectone/ui/theme/screens/Login/Login.kt
@@ -0,0 +1,127 @@
+package com.kimm.dreioprojectone.ui.theme.screens.Login
+
+import android.graphics.drawable.Icon
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+i
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text as Text
+
+@RequiresApi(Build.VERSION_CODES.M)
+@Composable
+fun LoginScreen() {
+
+    var name by remember { mutableStateOf(TextFieldValue("")) }
+    var email by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+    var context = LocalContext.current
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(Color.Magenta)
+
+    ){
+        Text(text = "Since you have registered this page.You are required to LogIn.",
+            color = Color.Black,
+            fontSize = 30.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "Login to your Account",
+            color = Color.White,
+            fontSize = 20.sp,
+            fontFamily = FontFamily.SansSerif
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "email"
+                )
+            },
+            label = {
+                Text(
+                    text = " Email Address ",
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Black
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .background(Color.White),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions()
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "pass"
+                )
+            },
+            label = {
+                Text(
+                    text = "Enter Password ",
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Black
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+                .background(Color.White),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions()
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+
+
+    }
+
+    }
+
